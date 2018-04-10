d58c88ba2d4f6695200d18ab02a605083099dff4
Mc-fly82 <flavius_marc@yahoo.fr>
2018-04-10 13:00:09 +0200

    add email to mail

--- resources/views/emails/forwardContactMessage.blade.php
+++ resources/views/emails/forwardContactMessage.blade.php
@@ -2,13 +2,18 @@
 # De
 
 {{$request->input('name')}}
+
+# Adresse de contact
+
+{{$request->input('name')}}
+
 # Sujet
 
 {{$request->input('subject')}}
+
 # Contenu
 
 {{$request->input('message')}}
-
 
 {{ config('app.name') }}
 @endcomponent
