ec65f7e9ea83a6432adb6feeee4a0f84894ad8a7
Mc-fly82 <flavius_marc@yahoo.fr>
2018-04-13 17:57:36 +0200

    add adresse + phone

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'operations-mavama.tech'),
+        'address' => env('MAIL_FROM_ADDRESS', 'operations@mavama.tech'),
         'name' => env('MAIL_FROM_NAME', 'mavama-technologie'),
     ],
 
